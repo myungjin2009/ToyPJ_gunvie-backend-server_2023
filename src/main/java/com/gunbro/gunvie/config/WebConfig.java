993be6e3c17652389babf9e8cc2e8761fec21b7c
Kim Myeongjin <myungjin2009@naver.com>
2023-06-01 21:36:10 +0900

    Fix: WebConfig Todo 주석 작성

--- src/main/java/com/gunbro/gunvie/config/WebConfig.java
+++ src/main/java/com/gunbro/gunvie/config/WebConfig.java
@@ -7,12 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    //프론트엔드 측 로컬 개발 시 CORS 문제가 발생하여 , allowedOrigins에 "*"가 들어가있음. 보안취약
+    //TODO allowedOrigins "*"를 제외할 방법
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://gunvie.vercel.app","https://gunvie.vercel.app","116.43.252.74","*")
                 .allowCredentials(true);
-        //.allowedOrigins("http://gunvie.vercel.app","https://gunvie.vercel.app","116.43.252.74","*")
     }
 
     @Override
